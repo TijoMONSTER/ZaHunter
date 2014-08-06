057de584a3261cf514fb2337c7e602cc07e8a97e
Iván Mervich <imervich@gmail.com>
2014-08-06 16:05:23 -0500

    Added tableView to show a list of near pizza restaurants

--- ZaHunter/ViewController.m
+++ ZaHunter/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property CLLocationManager *locationManager;
+@property NSArray *pizzaRestaurants;
+
+@property UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -17,13 +25,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+	self.locationManager = [CLLocationManager new];
+	self.locationManager.delegate = self;
+
+	self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+	self.activityIndicator.center = self.view.center;
+
+	[self.locationManager startUpdatingLocation];
+	[self.activityIndicator startAnimating];
+	[self.view addSubview:self.activityIndicator];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)reverseGeocode:(CLLocation *)location
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	CLGeocoder *geocoder = [CLGeocoder new];
+	[geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+		CLPlacemark *placemark = [placemarks firstObject];
+		[self findLocationsNear:placemark.location];
+	}];
+}
+
+- (void)findLocationsNear:(CLLocation *)location
+{
+	MKLocalSearchRequest *request = [MKLocalSearchRequest new];
+	request.naturalLanguageQuery = @"Pizza";
+	request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1));
+
+	MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
+	[search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+		self.pizzaRestaurants = response.mapItems;
+		[self.tableView reloadData];
+
+		[self.activityIndicator stopAnimating];
+		[self.activityIndicator removeFromSuperview];
+	}];
+}
+
+#pragma mark CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+	for (CLLocation *location in locations) {
+		[self.locationManager stopUpdatingLocation];
+		[self reverseGeocode:location];
+	}
+}
+
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return [self.pizzaRestaurants count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
+	MKMapItem *mapItem = self.pizzaRestaurants[indexPath.row];
+
+	cell.textLabel.text = mapItem.name;
+
+	return cell;
 }
 
 @end
